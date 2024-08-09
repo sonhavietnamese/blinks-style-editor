c79bfca37d88048c614c2b97998f43c54d695e80
sonhavietnamese <nguyensonha.hanz@gmail.com>
2024-08-09 17:47:06 +0700

    feat: layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,195 @@
-import Image from "next/image";
+'use client'
+
+import { ICONS } from '@/components/icons'
+import * as Popover from '@radix-ui/react-popover'
+import { ChangeEvent, useCallback, useEffect, useState } from 'react'
+import { HexAlphaColorPicker } from 'react-colorful'
 
 export default function Home() {
+  const [value, setValue] = useState(0)
+
+  const onInputChange = useCallback((ev: ChangeEvent<HTMLInputElement>) => {
+    const inputValue = parseInt(ev.target.value, 10)
+    if (inputValue >= 0 && inputValue <= 64) {
+      setValue(inputValue)
+    }
+  }, [])
+
+  const [snapshot, setSnapshot] = useState(value)
+
+  const [startVal, setStartVal] = useState(0)
+
+  const onStart = useCallback(
+    (event: React.MouseEvent<HTMLDivElement>) => {
+      setStartVal(event.clientX)
+      setSnapshot(value)
+    },
+    [value],
+  )
+
+  useEffect(() => {
+    const onUpdate = (evt: MouseEvent) => {
+      if (startVal) {
+        setValue(evt.clientX - snapshot)
+      }
+    }
+
+    const onEnd = () => {
+      setStartVal(0)
+    }
+
+    document.addEventListener('mousemove', onUpdate)
+    document.addEventListener('mouseup', onEnd)
+    return () => {
+      document.removeEventListener('mousemove', onUpdate)
+      document.removeEventListener('mouseup', onEnd)
+    }
+  }, [startVal, setValue, snapshot])
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className='grid text-[#373737] grid-cols-[18rem_1fr] w-screen relative background-dot h-screen p-4 bg-[#f7f7f7]'>
+      {/* <aside className='absolute top-5 right-5 bg-white p-5'>
+        <button>lg</button>
+        <button>xl</button>
+        <button>2xl</button>
+      </aside> */}
+
+      <aside className='flex flex-col w-full h-full p-4 rounded-[12px] shadow-sm col-span-1 bg-[#fff]'>
+        <div className='flex items-center gap-x-1.5 text-sm'>
+          <figure className='w-4 h-4'>{ICONS.star}</figure>
+          <span>General</span>
         </div>
-      </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div className='flex flex-col '>
+          <span className='text-xs text-[#000000B3] mt-3'>Background</span>
+          <div className='flex gap-2 mt-2'>
+            <button className='w-5 aspect-square rounded-full bg-white border border-[hsl(0_0%_92%)]'></button>
+            <button className='w-5 aspect-square rounded-full bg-black border border-[hsl(0_0%_92%)]'></button>
+          </div>
+        </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <div className='flex flex-col w-full'>
+          <label className='text-xs text-[#000000B3] mt-3'>Border Radius</label>
+          <div className='flex gap-2 mt-2'>
+            <fieldset className='relative w-full'>
+              <div onMouseDown={onStart} className='z-[2] cursor-ew-resize absolute top-0 bottom-0 flex items-center px-3 text-[12px]'>
+                <svg width='9' height='9' viewBox='0 0 9 9' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                  <path
+                    fill-rule='evenodd'
+                    clip-rule='evenodd'
+                    d='M9 1H5C2.79086 1 1 2.79086 1 5V9H0V5C0 2.23858 2.23858 0 5 0H9V1Z'
+                    fill='currentColor'></path>
+                </svg>
+              </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+              <input
+                type='number'
+                value={value}
+                onChange={onInputChange}
+                min={0}
+                max={64}
+                className='block leading-3 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none pl-8 pr-10 text-sm p-2 w-full border border-[#e6e6e6] rounded-lg'
+              />
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+              <div onMouseDown={onStart} className='z-[2] cursor-ew-resize absolute top-0 bottom-0 flex right-0 items-center px-2 text-[12px]'>
+                <kbd className='p-1.5 py-[1px] text-[11px] rounded bg-[#ebebeb]'>rem</kbd>
+              </div>
+            </fieldset>
+          </div>
+        </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <div className='flex flex-col w-full'>
+          <label className='text-xs text-[#000000B3] mt-3'>Border Radius</label>
+          <div className='flex gap-2 mt-2'>
+            <fieldset className='relative w-full'>
+              <Popover.Root>
+                <Popover.Trigger>
+                  <button>
+                    <div className='z-[2] absolute top-0 bottom-0 flex items-center px-3 text-[12px]'>
+                      <svg width='12' height='12' viewBox='0 0 12 12' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                        <path
+                          d='M2 4C2 2.89543 2.89543 2 4 2H10C11.1046 2 12 2.89543 12 4V10C12 11.1046 11.1046 12 10 12H4C2.89543 12 2 11.1046 2 10V4Z'
+                          fill='#E6E6E6'></path>
+                        <path
+                          d='M0.5 2C0.5 1.17157 1.17157 0.5 2 0.5H8C8.82843 0.5 9.5 1.17157 9.5 2V8C9.5 8.82843 8.82843 9.5 8 9.5H2C1.17157 9.5 0.5 8.82843 0.5 8V2Z'
+                          fill='white'
+                          stroke='#BFBFBF'></path>
+                      </svg>
+                    </div>
+
+                    <input
+                      value={'16 16 16 16'}
+                      readOnly
+                      className='block cursor-pointer leading-3 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none pl-8  text-sm p-2 w-full border border-[#e6e6e6] rounded-lg'
+                    />
+                  </button>
+                </Popover.Trigger>
+                <Popover.Portal>
+                  <Popover.Content className='PopoverContent'>
+                    <div style={{ display: 'flex', flexDirection: 'column', gap: 10 }}>
+                      <p className='Text' style={{ marginBottom: 10 }}>
+                        Dimensions
+                      </p>
+                      <fieldset className='Fieldset'>
+                        <label className='Label' htmlFor='width'>
+                          Width
+                        </label>
+                        <input className='Input' id='width' defaultValue='100%' />
+                      </fieldset>
+                      <fieldset className='Fieldset'>
+                        <label className='Label' htmlFor='maxWidth'>
+                          Max. width
+                        </label>
+                        <input className='Input' id='maxWidth' defaultValue='300px' />
+                      </fieldset>
+                      <fieldset className='Fieldset'>
+                        <label className='Label' htmlFor='height'>
+                          Height
+                        </label>
+                        <input className='Input' id='height' defaultValue='25px' />
+                      </fieldset>
+                      <fieldset className='Fieldset'>
+                        <label className='Label' htmlFor='maxHeight'>
+                          Max. height
+                        </label>
+                        <input className='Input' id='maxHeight' defaultValue='none' />
+                      </fieldset>
+                    </div>
+
+                    <Popover.Arrow className='PopoverArrow' />
+                  </Popover.Content>
+                </Popover.Portal>
+              </Popover.Root>
+            </fieldset>
+
+            <fieldset className='relative w-full'>
+              <Popover.Root>
+                <Popover.Trigger asChild className=''>
+                  <div className='z-[2] absolute top-0 bottom-0 flex items-center px-2 text-[12px]'>
+                    <div className='w-5 h-5 aspect-square rounded bg-red-400'></div>
+                  </div>
+                </Popover.Trigger>
+                <Popover.Portal>
+                  <Popover.Content className='PopoverContent'>
+                    <div style={{ display: 'flex', flexDirection: 'column', gap: 10 }}>
+                      <HexAlphaColorPicker />
+                    </div>
+
+                    <Popover.Arrow className='PopoverArrow' />
+                  </Popover.Content>
+                </Popover.Portal>
+              </Popover.Root>
+
+              <input
+                value={'#ffffff'}
+                className='block leading-3 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none pl-[34px] text-sm p-2 w-full border border-[#e6e6e6] rounded-lg'
+              />
+            </fieldset>
+          </div>
+        </div>
+      </aside>
+
+      <div className='col-span-1'>{/* <Blink stylePreset='x-dark' /> */}</div>
     </main>
-  );
+  )
 }
